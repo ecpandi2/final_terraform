6239dfdc26b33cfba1480f4e3fa40c10282dfcf9
Pandiyan <pandiec@gmail.com>
2022-07-23 15:27:06 +0530

    C:/Program Files/Git/terraform-aws-s3-bucket

--- 3_terraform_remote_backend/infrastructure_modules/terraform_remote_backend/outputs.tf
+++ 3_terraform_remote_backend/infrastructure_modules/terraform_remote_backend/outputs.tf
@@ -1,12 +1,12 @@
 ## S3 ##
-output "s3_id" {
-  description = "The name of the table"
-  value       = module.s3_bucket_terraform_remote_backend.this_s3_bucket_id
+output "s3_bucket_id" {
+  description = "The name of the bucket."
+  value       = try(aws_s3_bucket_policy.this[0].id, aws_s3_bucket.this[0].id, "")
 }
 
-output "s3_arn" {
-  description = "The arn of the table"
-  value       = module.s3_bucket_terraform_remote_backend.this_s3_bucket_arn
+output "s3_bucket_arn" {
+  description = "The ARN of the bucket. Will be of format arn:aws:s3:::bucketname."
+  value       = try(aws_s3_bucket.this[0].arn, "")
 }
 
 ## DynamoDB ##
