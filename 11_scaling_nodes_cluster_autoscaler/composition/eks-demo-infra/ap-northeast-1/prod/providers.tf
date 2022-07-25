60f2d7974013dac78a5c644e572a183d1169fc75
Pandiyan <pandiec@gmail.com>
2022-07-25 16:24:23 +0530

    Updated the kubernetes cluster

--- 11_scaling_nodes_cluster_autoscaler/composition/eks-demo-infra/ap-northeast-1/prod/providers.tf
+++ 11_scaling_nodes_cluster_autoscaler/composition/eks-demo-infra/ap-northeast-1/prod/providers.tf
@@ -31,12 +31,12 @@ provider "aws" {
 # ref: https://registry.terraform.io/providers/hashicorp/kubernetes/latest/docs
 provider "kubernetes" {
   # if you use default value of "manage_aws_auth = true" then you need to configure the kubernetes provider as per the doc: https://github.com/terraform-aws-modules/terraform-aws-eks/blob/v12.1.0/README.md#conditional-creation, https://github.com/terraform-aws-modules/terraform-aws-eks/issues/911
-  host                   = element(concat(data.aws_eks_cluster.cluster[*].endpoint, tolist([""])), 0)
-  cluster_ca_certificate = base64decode(element(concat(data.aws_eks_cluster.cluster[*].certificate_authority.0.data, tolist([""])), 0))
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
   token                  = element(concat(data.aws_eks_cluster_auth.cluster[*].token, tolist([""])), 0)
 #  load_config_file       = false # set to false unless you want to import local kubeconfig to terraform
  exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
+      api_version = "client.authentication.k8s.io/v1"
       args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_id]
       command     = "aws"
     }
@@ -45,12 +45,12 @@ provider "kubernetes" {
 provider "helm" {
    kubernetes {
        # if you use default value of "manage_aws_auth = true" then you need to configure the kubernetes provider as per the doc: https://github.com/terraform-aws-modules/terraform-aws-eks/blob/v12.1.0/README.md#conditional-creation, https://github.com/terraform-aws-modules/terraform-aws-eks/issues/911
-      host                   = element(concat(data.aws_eks_cluster.cluster[*].endpoint, tolist([""])), 0)
-      cluster_ca_certificate = base64decode(element(concat(data.aws_eks_cluster.cluster[*].certificate_authority.0.data, tolist([""])), 0))
-      token                  = element(concat(data.aws_eks_cluster_auth.cluster[*].token, tolist([""])), 0)
-        #  load_config_file       = false # set to false unless you want to import local kubeconfig to terraform
-    exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
+      host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  token                  = element(concat(data.aws_eks_cluster_auth.cluster[*].token, tolist([""])), 0)
+#  load_config_file       = false # set to false unless you want to import local kubeconfig to terraform
+ exec {
+      api_version = "client.authentication.k8s.io/v1"
       args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_id]
       command     = "aws"
     }
