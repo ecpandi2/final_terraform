2717149f86db46dd8686b4b75f3ab5396832ac1a
Pandiyan <pandiec@gmail.com>
2022-07-25 21:12:48 +0530

    Adding new hosted zone

--- 11_scaling_nodes_cluster_autoscaler/resource_modules/deployments/acm.tf
+++ 11_scaling_nodes_cluster_autoscaler/resource_modules/deployments/acm.tf
@@ -1,6 +1,6 @@
 # Resource: ACM Certificate
 resource "aws_acm_certificate" "acm_cert" {
-  domain_name       = "*.cmcloudlab1624.info"
+  domain_name       = "*.cmcloudlab711.info"
   validation_method = "DNS"
 
   tags = {
