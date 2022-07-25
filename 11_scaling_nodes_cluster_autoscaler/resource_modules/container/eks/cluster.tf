f160951765c64706020534375295abf3b8bc3615
Pandiyan <pandiec@gmail.com>
2022-07-25 22:08:25 +0530

    Adding the Autoscaler driver

--- 11_scaling_nodes_cluster_autoscaler/resource_modules/container/eks/cluster.tf
+++ 11_scaling_nodes_cluster_autoscaler/resource_modules/container/eks/cluster.tf
@@ -493,4 +493,118 @@ resource "helm_release" "external_dns" {
     value = "sync"   # "sync" will ensure that when ingress resource is deleted, equivalent DNS record in Route53 will get deleted
   }    
         
+}# Resource: IAM Policy for Cluster Autoscaler
+resource "aws_iam_policy" "cluster_autoscaler_iam_policy" {
+  name        = "${local.name}-AmazonEKSClusterAutoscalerPolicy"
+  path        = "/"
+  description = "EKS Cluster Autoscaler Policy"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": [
+                "autoscaling:DescribeAutoScalingGroups",
+                "autoscaling:DescribeAutoScalingInstances",
+                "autoscaling:DescribeInstances",
+                "autoscaling:DescribeLaunchConfigurations",
+                "autoscaling:DescribeTags",
+                "autoscaling:SetDesiredCapacity",
+                "autoscaling:TerminateInstanceInAutoScalingGroup",
+                "ec2:DescribeLaunchTemplateVersions",
+                "ec2:DescribeInstanceTypes"
+            ],
+            "Resource": "*",
+            "Effect": "Allow"
+        }
+    ]
+})
+}
+
+# Resource: IAM Role for Cluster Autoscaler
+## Create IAM Role and associate it with Cluster Autoscaler IAM Policy
+resource "aws_iam_role" "cluster_autoscaler_iam_role" {
+  name = "${local.name}-cluster-autoscaler"
+
+  # Terraform's "jsonencode" function converts a Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Federated = var.enable_irsa ? concat(aws_iam_openid_connect_provider.oidc_provider[*].arn, [""])[0] : null
+        }
+        Condition = {
+          StringEquals = {
+            "${local.aws_iam_oidc_connect_provider_extract_from_arn}:sub": "system:serviceaccount:kube-system:cluster-autoscaler"
+          }
+        }        
+      },
+    ]
+  })
+
+  tags = {
+    tag-key = "cluster-autoscaler"
+  }
+}
+
+
+# Associate IAM Policy to IAM Role
+resource "aws_iam_role_policy_attachment" "cluster_autoscaler_iam_role_policy_attach" {
+  policy_arn = aws_iam_policy.cluster_autoscaler_iam_policy.arn 
+  role       = aws_iam_role.cluster_autoscaler_iam_role.name
+}
+
+# Install Cluster Autoscaler using HELM
+
+# Resource: Helm Release 
+resource "helm_release" "cluster_autoscaler_release" {
+  depends_on = [aws_iam_role.cluster_autoscaler_iam_role ]            
+  name       = "${local.name}-ca"
+
+  repository = "https://kubernetes.github.io/autoscaler"
+  chart      = "cluster-autoscaler"
+
+  namespace = "kube-system"   
+
+  set {
+    name  = "cloudProvider"
+    value = "aws"
+  }
+
+  set {
+    name  = "autoDiscovery.clusterName"
+    value = element(concat(aws_eks_cluster.this.*.id, tolist([""])), 0)
+  }
+
+  set {
+    name  = "awsRegion"
+    value = var.aws_region
+  }
+
+  set {
+    name  = "rbac.serviceAccount.create"
+    value = "true"
+  }
+
+  set {
+    name  = "rbac.serviceAccount.name"
+    value = "cluster-autoscaler"
+  }
+
+  set {
+    name  = "rbac.serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+    value = "${aws_iam_role.cluster_autoscaler_iam_role.arn}"
+  }
+  # Additional Arguments (Optional) - To Test How to pass Extra Args for Cluster Autoscaler
+  #set {
+  #  name = "extraArgs.scan-interval"
+  #  value = "20s"
+  #}    
+   
 }
--- 11_scaling_nodes_cluster_autoscaler/resource_modules/container/eks/outputs.tf
+++ 11_scaling_nodes_cluster_autoscaler/resource_modules/container/eks/outputs.tf
@@ -228,3 +228,14 @@ output "externaldns_helm_metadata" {
   description = "Metadata Block outlining status of the deployed release."
   value = helm_release.external_dns.metadata
 }
+
+output "cluster_autoscaler_iam_role_arn" {
+  description = "Cluster Autoscaler IAM Role ARN"
+  value = aws_iam_role.cluster_autoscaler_iam_role.arn
+}
+
+# Helm Release Outputs
+output "cluster_autoscaler_helm_metadata" {
+  description = "Metadata Block outlining status of the deployed release."
+  value = helm_release.cluster_autoscaler_release.metadata
+}
