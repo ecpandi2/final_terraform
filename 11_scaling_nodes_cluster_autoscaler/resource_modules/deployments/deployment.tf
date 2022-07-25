6baee4eca948df81bd16a5b6df642cb2ce8a8e5a
Pandiyan <pandiec@gmail.com>
2022-07-25 23:09:28 +0530

    Adding final for cloudwatch

--- 11_scaling_nodes_cluster_autoscaler/resource_modules/deployments/deployment.tf
+++ 11_scaling_nodes_cluster_autoscaler/resource_modules/deployments/deployment.tf
@@ -1,43 +1,17 @@
-# Kubernetes Deployment Manifest
-resource "kubernetes_deployment_v1" "vpa_demo_app" {
+## Resource: Namespace
+resource "kubernetes_namespace_v1" "amazon_cloudwatch" {
   metadata {
-    name = "vpa-demo-deployment" 
-    labels = {
-      app = "vpa-nginx"
-    }
-  } 
- 
-  spec {
-    replicas = 4
+    name = "amazon-cloudwatch"
+  }
+}
 
-    selector {
-      match_labels = {
-        app = "vpa-nginx"
-      }
-    }
-
-    template {
-      metadata {
-        labels = {
-          app = "vpa-nginx"
-        }
-      }
-
-      spec {
-        container {
-          image = "stacksimplify/kubenginx:1.0.0"
-          name  = "vpa-nginx"
-          port {
-            container_port = 80
-          }
-          resources {
-            requests = {
-              cpu = "5m"
-              memory = "5Mi"
-            }
-          }
-          }
-        }
-      }
-    }
+# Resource: kubectl_manifest which will create k8s Resources from the URL specified in above datasource
+resource "kubectl_manifest" "fluentbit_resources" {
+  depends_on = [
+    kubernetes_namespace_v1.amazon_cloudwatch,
+    kubernetes_config_map_v1.fluentbit_configmap,
+    kubectl_manifest.cwagent_daemonset
+    ]
+  for_each = data.kubectl_file_documents.fluentbit_docs.manifests    
+  yaml_body = each.value
 }
