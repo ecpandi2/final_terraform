d4f06fa8c976e6dd8cd039c83ed6a888e27b155b
Pandiyan <pandiec@gmail.com>
2022-07-25 21:22:27 +0530

    External DNS controller

--- 11_scaling_nodes_cluster_autoscaler/resource_modules/container/eks/outputs.tf
+++ 11_scaling_nodes_cluster_autoscaler/resource_modules/container/eks/outputs.tf
@@ -213,3 +213,18 @@ output "lbc_helm_metadata" {
   description = "Metadata Block outlining status of the deployed release."
   value = helm_release.loadbalancer_controller.metadata
 }
+
+output "externaldns_iam_policy_arn" {
+  value = aws_iam_policy.externaldns_iam_policy.arn 
+} 
+
+output "externaldns_iam_role_arn" {
+  description = "External DNS IAM Role ARN"
+  value = aws_iam_role.externaldns_iam_role.arn
+}
+
+# Helm Release Outputs
+output "externaldns_helm_metadata" {
+  description = "Metadata Block outlining status of the deployed release."
+  value = helm_release.external_dns.metadata
+}
