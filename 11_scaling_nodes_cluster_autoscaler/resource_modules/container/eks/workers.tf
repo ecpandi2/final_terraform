0ef9d36ecd0a9a04de05e133847419112a7fffe7
Pandiyan <pandiec@gmail.com>
2022-07-25 20:00:51 +0530

    Added

--- 11_scaling_nodes_cluster_autoscaler/resource_modules/container/eks/workers.tf
+++ 11_scaling_nodes_cluster_autoscaler/resource_modules/container/eks/workers.tf
@@ -294,7 +294,9 @@ resource "aws_launch_configuration" "workers" {
     aws_iam_role_policy_attachment.workers_AmazonEKSWorkerNodePolicy,
     aws_iam_role_policy_attachment.workers_AmazonEKS_CNI_Policy,
     aws_iam_role_policy_attachment.workers_AmazonEC2ContainerRegistryReadOnly,
-    aws_iam_role_policy_attachment.workers_additional_policies
+    aws_iam_role_policy_attachment.workers_additional_policies,
+    aws_eks_cluster.this,
+    kubernetes_config_map.aws_auth
   ]
 }
 
